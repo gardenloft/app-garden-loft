cb66c2d2c88a0d171163a721e5057cdbc3817ffd
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-05-22 12:17:59 -0600

    installed react-dom

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Garden Loft App</Text>
+      <Text>Garden Loft App yo</Text>
       <StatusBar style="auto" />
     </View>
   );
