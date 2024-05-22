094c8e036c10afb3ec0720d9a44341340a547789
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-05-22 14:05:36 -0600

    create components

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const GLImage = require("./assets/garden-loft-logo2.png")
 
 export default function App() {
   return (
+    <>
+    <View style={styles.wholecontainer}>
     <View style={styles.container}>
-      <Text>Garden Loft App yo</Text>
+      <Image source={GLImage} style={styles.image} />
+      <Text style={{color: "darkgrey", fontSize: 50, fontFamily: "Courier" }}>Welcome To</Text>
+      <Text style={{color: "orange", fontSize: 50, fontFamily: "Courier" }}>Garden Loft</Text>
       <StatusBar style="auto" />
     </View>
+    <View style={styles.container}>
+    <Image source={GLImage} style={styles.image} />
+    <Text style={{color: "darkgrey", fontSize: 50, fontFamily: "Courier" }}>Welcome To</Text>
+    <Text style={{color: "orange", fontSize: 50, fontFamily: "Courier" }}>Garden Loft</Text>
+    <StatusBar style="auto" />
+  </View>
+  </View>
+  </>
   );
 }
 
 const styles = StyleSheet.create({
+  wholecontainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FCF8E3',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 400,
+    height: 200,
+    marginBottom: 50,
+  }
 });
