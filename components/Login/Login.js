bf89f348b9f3ce9a7fe644d0da28e3d1b9669d43
whimsical-phoenix <shahprapti12@gmail.com>
2024-05-27 16:43:00 -0600

    PChanges for expo51

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -70,10 +70,10 @@
 //       <StatusBar style="auto" />
 
 //       {/* <Text style={styles.label}>Enter your name:</Text> */}
-//       <TextInput 
-//         style={styles.input} 
-//         value={name} 
-//         onChangeText={setName} 
+//       <TextInput
+//         style={styles.input}
+//         value={name}
+//         onChangeText={setName}
 //         placeholder="Enter Your Name"
 //       />
 //       {error ? <Text style={styles.error}>{error}</Text> : null}
@@ -126,26 +126,27 @@ import {
   Dimensions,
 } from "react-native";
 import React, { useState, useEffect } from "react";
-import CheckBox from 'expo-checkbox'; // Import CheckBox from expo-checkbox
+import CheckBox from "expo-checkbox"; // Import CheckBox from expo-checkbox
 import { FIREBASE_AUTH, FIRESTORE_DB } from "../../FirebaseConfig";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { doc, setDoc } from 'firebase/firestore';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "expo-router/build/useNavigation";
 
-
-
-const { width: viewportWidth, height: viewportHeight } = Dimensions.get("window");
+const { width: viewportWidth, height: viewportHeight } =
+  Dimensions.get("window");
 
 const Login = ({}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState("");
   const [loading, setLoading] = useState(false);
   const [rememberMe, setRememberMe] = useState(false); // Remember Me state
   const auth = FIREBASE_AUTH;
   const navigation = useNavigation();
-
-
 
   useEffect(() => {
     const checkRememberedUser = async () => {
@@ -169,9 +170,16 @@ const Login = ({}) => {
   const signIn = async () => {
     setLoading(true);
     try {
-      const response = await signInWithEmailAndPassword(auth, email.trim(), password);
+      const response = await signInWithEmailAndPassword(
+        auth,
+        email.trim(),
+        password
+      );
       if (rememberMe) {
-        await AsyncStorage.setItem("rememberedUser", JSON.stringify({ email, password }));
+        await AsyncStorage.setItem(
+          "rememberedUser",
+          JSON.stringify({ email, password })
+        );
       }
       console.log(response);
       navigation.navigate("home"); // Navigate to CarouselOne screen
@@ -186,13 +194,17 @@ const Login = ({}) => {
   const signUp = async () => {
     setLoading(true); // Start loading
     try {
-      const response = await createUserWithEmailAndPassword(auth, email.trim(), password);
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email.trim(),
+        password
+      );
       const user = response.user;
       await setDoc(doc(FIRESTORE_DB, "users", user.uid), {
         email: email,
-        // name: name,
+        userName: userName,
       });
-      alert('Account created successfully! Check your email.');
+      alert("Account created successfully! Check your email.");
       // navigation.navigate("Home"); // Navigate to home  screen
     } catch (error) {
       console.error(error);
@@ -206,21 +218,26 @@ const Login = ({}) => {
     <View style={styles.container}>
       <KeyboardAvoidingView behavior="padding">
         <Text style={styles.welcome}>Welcome Garden Loft Residents</Text>
+
+        <TextInput
+          value={userName}
+          style={styles.input}
+          placeholder="name"
+          autoCapitalize="none"
+          onChangeText={(text) => setUserName(text)}></TextInput>
         <TextInput
           value={email}
           style={styles.input}
           placeholder="email"
           autoCapitalize="none"
-          onChangeText={(text) => setEmail(text)}
-        ></TextInput>
+          onChangeText={(text) => setEmail(text)}></TextInput>
         <TextInput
           secureTextEntry={true}
           value={password}
           style={styles.input}
           placeholder="password"
           autoCapitalize="none"
-          onChangeText={(text) => setPassword(text)}
-        ></TextInput>
+          onChangeText={(text) => setPassword(text)}></TextInput>
         <View style={styles.checkboxContainer}>
           <CheckBox
             value={rememberMe}
@@ -252,7 +269,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#FCF8E3",
     width: viewportWidth * 1,
     height: viewportHeight * 1,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
   input: {
     borderColor: "black",
@@ -260,21 +277,21 @@ const styles = StyleSheet.create({
     padding: 30,
     width: viewportWidth * 0.4,
     height: viewportHeight * 0.1,
-    alignSelf: 'center',
+    alignSelf: "center",
     borderRadius: 30,
     marginBottom: 30,
   },
   welcome: {
     fontSize: 40,
     color: "#f09030",
-    alignSelf: 'center',
+    alignSelf: "center",
     marginBottom: 30,
   },
   checkboxContainer: {
     flexDirection: "row",
     alignItems: "center",
     marginBottom: 30,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   checkbox: {
     alignSelf: "center",
