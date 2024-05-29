65d46ebdf655a893aeea328d036fcd7951ef5d21
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-05-29 15:11:59 -0600

    removed RNFirebase packages

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import Login from '../components/Login/Login';
 
 export default function App() {
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
 });
 
