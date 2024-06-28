7be95025866de91c0b7206e1a5184ef03c1496f2
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-06-27 19:01:16 -0600

    push video call code

--- app/home.js
+++ app/home.js
@@ -4,7 +4,10 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import HelpButton from "../components/HelpButton";
 import Carousel from "../components/CarouselOne/Carousel";
 
+
+
 const App = () => (
+  
   <GestureHandlerRootView style={{ flex: 1, backgroundColor: "#FCF8E3" }}>
     <SafeAreaProvider>
       <HelpButton />
