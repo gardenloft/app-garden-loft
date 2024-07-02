b168450a9ffb4bb3d850fb31581e35d1a145d33b
Meseret <mesiteklu2018@gmail.com>
2024-07-02 17:09:26 -0600

    change icons

--- app/VideoSDK2.js
+++ app/VideoSDK2.js
@@ -296,262 +296,262 @@
 //   );
 // }
 
-// import React, { useState, useEffect } from 'react';
-// import {
-//   SafeAreaView,
-//   TouchableOpacity,
-//   Text,
-//   View,
-//   FlatList,
-//   Dimensions,
-//   Alert,
-// } from 'react-native';
-// import {
-//   MeetingProvider,
-//   useMeeting,
-//   useParticipant,
-//   MediaStream,
-//   RTCView,
-// } from '@videosdk.live/react-native-sdk';
-// import { createMeeting, token } from '../components/api';
-// import { FIRESTORE_DB } from '../FirebaseConfig';
-// import { doc, getDoc, setDoc } from "firebase/firestore";
-// import * as Notifications from 'expo-notifications';
-// import { getAuth } from 'firebase/auth';
-// import { useLocalSearchParams, useRouter} from 'expo-router';
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  Text,
+  View,
+  FlatList,
+  Dimensions,
+  Alert,
+} from 'react-native';
+import {
+  MeetingProvider,
+  useMeeting,
+  useParticipant,
+  MediaStream,
+  RTCView,
+} from '@videosdk.live/react-native-sdk';
+import { createMeeting, token } from '../components/api';
+import { FIRESTORE_DB } from '../FirebaseConfig';
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import * as Notifications from 'expo-notifications';
+import { getAuth } from 'firebase/auth';
+import { useLocalSearchParams, useRouter} from 'expo-router';
 
-// const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
 
-// Notifications.setNotificationHandler({
-//   handleNotification: async () => ({
-//     shouldShowAlert: true,
-//     shouldPlaySound: true,
-//     shouldSetBadge: false,
-//   }),
-// });
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
-// function JoinScreen(props) {
-//   const [meetingVal, setMeetingVal] = useState('');
-//   return (
-//     <SafeAreaView
-//       style={{
-//         flex: 1,
-//         backgroundColor: '#F6F6FF',
-//         justifyContent: 'center',
-//         paddingHorizontal: 60,
-//         width: viewportWidth * 0.8,
-//       }}>
+function JoinScreen(props) {
+  const [meetingVal, setMeetingVal] = useState('');
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: '#F6F6FF',
+        justifyContent: 'center',
+        paddingHorizontal: 60,
+        width: viewportWidth * 0.8,
+      }}>
      
-//     </SafeAreaView>
-//   );
-// }
+    </SafeAreaView>
+  );
+}
 
-// const Button = ({ onPress, buttonText, backgroundColor }) => {
-//   return (
-//     <TouchableOpacity
-//       onPress={onPress}
-//       style={{
-//         backgroundColor: backgroundColor,
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         padding: 16,
-//         borderRadius: 8,
-//         marginBottom: 40,
-//       }}>
-//       <Text style={{ color: 'white', fontSize: 16 }}>{buttonText}</Text>
-//     </TouchableOpacity>
-//   );
-// };
+const Button = ({ onPress, buttonText, backgroundColor }) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        backgroundColor: backgroundColor,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        borderRadius: 8,
+        marginBottom: 40,
+      }}>
+      <Text style={{ color: 'white', fontSize: 16 }}>{buttonText}</Text>
+    </TouchableOpacity>
+  );
+};
 
-// function ControlsContainer({ join, leave, toggleWebcam, toggleMic,back }) {
-//   return (
-//     <View
-//       style={{
-//         padding: 24,
-//         flexDirection: 'row',
-//         justifyContent: 'space-between',
-//         width: viewportWidth * 0.8,
-//         height: viewportHeight * 0.2,
-//       }}>
-//       <Button onPress={join} buttonText="Join" backgroundColor="#1178F8" />
-//       <Button onPress={toggleWebcam} buttonText="Toggle Webcam" backgroundColor="#1178F8" />
-//       <Button onPress={toggleMic} buttonText="Toggle Mic" backgroundColor="#1178F8" />
-//       <Button onPress={leave} buttonText="Leave" backgroundColor="#FF0000" />
-//       <Button onPress={back} buttonText="Back" backgroundColor="#FF0000" />
-//     </View>
-//   );
-// }
+function ControlsContainer({ join, leave, toggleWebcam, toggleMic,back }) {
+  return (
+    <View
+      style={{
+        padding: 24,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: viewportWidth * 0.8,
+        height: viewportHeight * 0.2,
+      }}>
+      <Button onPress={join} buttonText="Join" backgroundColor="#1178F8" />
+      <Button onPress={toggleWebcam} buttonText="Toggle Webcam" backgroundColor="#1178F8" />
+      <Button onPress={toggleMic} buttonText="Toggle Mic" backgroundColor="#1178F8" />
+      <Button onPress={leave} buttonText="Leave" backgroundColor="#FF0000" />
+      <Button onPress={back} buttonText="Back" backgroundColor="#FF0000" />
+    </View>
+  );
+}
 
-// function ParticipantView({ participantId }) {
-//   const { webcamStream, webcamOn } = useParticipant(participantId);
+function ParticipantView({ participantId }) {
+  const { webcamStream, webcamOn } = useParticipant(participantId);
 
-//   if (webcamOn && webcamStream) {
-//     return (
-//       <RTCView
-//         streamURL={new MediaStream([webcamStream.track]).toURL()}
-//         objectFit="cover"
-//         style={{
-//           height: 200,
-//           marginVertical: 16,
-//           marginHorizontal: 16,
-//         }}
-//       />
-//     );
-//   } else {
-//     return (
-//       <View
-//         style={{
-//           height: 400,
-//           marginVertical: 16,
-//           marginHorizontal: 16,
-//           backgroundColor: '#cccccc',
-//           justifyContent: 'center',
-//           alignItems: 'center',
-//         }}>
-//         <Text style={{ fontSize: 18 }}>Webcam is off</Text>
-//       </View>
-//     );
-//   }
-// }
+  if (webcamOn && webcamStream) {
+    return (
+      <RTCView
+        streamURL={new MediaStream([webcamStream.track]).toURL()}
+        objectFit="cover"
+        style={{
+          height: 200,
+          marginVertical: 16,
+          marginHorizontal: 16,
+        }}
+      />
+    );
+  } else {
+    return (
+      <View
+        style={{
+          height: 400,
+          marginVertical: 16,
+          marginHorizontal: 16,
+          backgroundColor: '#cccccc',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text style={{ fontSize: 18 }}>Webcam is off</Text>
+      </View>
+    );
+  }
+}
 
-// function ParticipantList({ participants }) {
-//   return participants.length > 0 ? (
-//     <FlatList
-//       data={participants}
-//       renderItem={({ item }) => <ParticipantView participantId={item} />}
-//     />
-//   ) : (
-//     <View
-//       style={{
-//         flex: 1,
-//         backgroundColor: '#F6F6FF',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//       }}>
-//       <Text style={{ fontSize: 24 }}>Press Join button to enter meeting.</Text>
-//     </View>
-//   );
-// }
+function ParticipantList({ participants }) {
+  return participants.length > 0 ? (
+    <FlatList
+      data={participants}
+      renderItem={({ item }) => <ParticipantView participantId={item} />}
+    />
+  ) : (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#F6F6FF',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text style={{ fontSize: 24 }}>Press Join button to enter meeting.</Text>
+    </View>
+  );
+}
 
-// function MeetingView({ autoJoin, setAutoJoin }) {
-//   const { join, leave, toggleWebcam, toggleMic, meetingId, participants } = useMeeting({});
-//   const participantsArrId = [...participants.keys()];
+function MeetingView({ autoJoin, setAutoJoin }) {
+  const { join, leave, toggleWebcam, toggleMic, meetingId, participants } = useMeeting({});
+  const participantsArrId = [...participants.keys()];
 
-//   const router = useRouter();
+  const router = useRouter();
 
-//   const handleBack = () => {
-//     router.back();
-//   };
+  const handleBack = () => {
+    router.back();
+  };
  
 
-//   useEffect(() => {
-//     if (autoJoin) {
-//       join();
-//       toggleWebcam();
-//       setAutoJoin(false);
-//     }
-//   }, [autoJoin, join, toggleWebcam, setAutoJoin]);
+  useEffect(() => {
+    if (autoJoin) {
+      join();
+      toggleWebcam();
+      setAutoJoin(false);
+    }
+  }, [autoJoin, join, toggleWebcam, setAutoJoin]);
 
-//   return (
-//     <View style={{ flex: 1 }}>
-//       {meetingId ? <Text style={{ fontSize: 22, padding: 16 }}>Meeting Id: {meetingId}</Text> : null}
-//       <ParticipantList participants={participantsArrId} />
-//       <ControlsContainer join={join} leave={leave} toggleWebcam={toggleWebcam} toggleMic={toggleMic} back={handleBack} />
-//     </View>
-//   );
-// }
+  return (
+    <View style={{ flex: 1 }}>
+      {meetingId ? <Text style={{ fontSize: 22, padding: 16 }}>Meeting Id: {meetingId}</Text> : null}
+      <ParticipantList participants={participantsArrId} />
+      <ControlsContainer join={join} leave={leave} toggleWebcam={toggleWebcam} toggleMic={toggleMic} back={handleBack} />
+    </View>
+  );
+}
 
-// export default function VideoSDK() {
-//   const [meetingId, setMeetingId] = useState(null);
-//   const [autoJoin, setAutoJoin] = useState(false);
-//   const params = useLocalSearchParams();
-//   const auth = getAuth();
-//   const user = auth.currentUser;
+export default function VideoSDK() {
+  const [meetingId, setMeetingId] = useState(null);
+  const [autoJoin, setAutoJoin] = useState(false);
+  const params = useLocalSearchParams();
+  const auth = getAuth();
+  const user = auth.currentUser;
 
-//   useEffect(() => {
-//     if (params.meetingId) {
-//       setMeetingId(params.meetingId);
-//       setAutoJoin(true);
-//     }
-//   }, [params]);
+  useEffect(() => {
+    if (params.meetingId) {
+      setMeetingId(params.meetingId);
+      setAutoJoin(true);
+    }
+  }, [params]);
 
-//   const getMeetingId = async id => {
-//     const newMeetingId = id == null ? await createMeeting({ token }) : id;
-//     setMeetingId(newMeetingId);
-//   };
+  const getMeetingId = async id => {
+    const newMeetingId = id == null ? await createMeeting({ token }) : id;
+    setMeetingId(newMeetingId);
+  };
 
-//   const callUser = async (calleeUid) => {
-//     const newMeetingId = await createMeeting({ token });
-//     setMeetingId(newMeetingId);
-//     setAutoJoin(true);
+  const callUser = async (calleeUid) => {
+    const newMeetingId = await createMeeting({ token });
+    setMeetingId(newMeetingId);
+    setAutoJoin(true);
    
 
-//     const callerDoc = await getDoc(doc(FIRESTORE_DB, 'users', user.uid));
-//     const caller = callerDoc.data().userName;
+    const callerDoc = await getDoc(doc(FIRESTORE_DB, 'users', user.uid));
+    const caller = callerDoc.data().userName;
 
-//     const calleeDoc = await getDoc(doc(FIRESTORE_DB, 'users', calleeUid));
-//     if (!calleeDoc.exists()) {
-//       Alert.alert('Callee not found');
-//       return;
-//     }
-//     const calleeData = calleeDoc.data();
-//     const calleePushToken = calleeData.pushToken;
+    const calleeDoc = await getDoc(doc(FIRESTORE_DB, 'users', calleeUid));
+    if (!calleeDoc.exists()) {
+      Alert.alert('Callee not found');
+      return;
+    }
+    const calleeData = calleeDoc.data();
+    const calleePushToken = calleeData.pushToken;
 
-//     await setDoc(doc(FIRESTORE_DB, 'users', user.uid), { callerId: newMeetingId }, { merge: true });
-//     await setDoc(doc(FIRESTORE_DB, 'users', calleeUid), { calleeId: newMeetingId }, { merge: true });
+    await setDoc(doc(FIRESTORE_DB, 'users', user.uid), { callerId: newMeetingId }, { merge: true });
+    await setDoc(doc(FIRESTORE_DB, 'users', calleeUid), { calleeId: newMeetingId }, { merge: true });
 
-//     const message = {
-//       to: calleePushToken,
-//       sound: 'default',
-//       title: 'Incoming Call',
-//       body: `${caller} is calling you`,
-//       data: { meetingId: newMeetingId },
-//       categoryId: 'incoming_call',
-//     };
+    const message = {
+      to: calleePushToken,
+      sound: 'default',
+      title: 'Incoming Call',
+      body: `${caller} is calling you`,
+      data: { meetingId: newMeetingId },
+      categoryId: 'incoming_call',
+    };
 
-//     await fetch('https://exp.host/--/api/v2/push/send', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Accept: 'application/json',
-//       },
-//       body: JSON.stringify(message),
-//     });
+    await fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(message),
+    });
 
-//     joinMeeting(newMeetingId);
-//   };
+    joinMeeting(newMeetingId);
+  };
 
-//   const joinMeeting = async meetingId => {
-//     console.log('Joining meeting with ID:', meetingId);
-//   };
+  const joinMeeting = async meetingId => {
+    console.log('Joining meeting with ID:', meetingId);
+  };
 
-//   useEffect(() => {
-//     if (params.calleeUid) {
-//       callUser(params.calleeUid);
-//       setAutoJoin(true); 
-//     }
-//   }, [params.calleeUid]);
+  useEffect(() => {
+    if (params.calleeUid) {
+      callUser(params.calleeUid);
+      setAutoJoin(true); 
+    }
+  }, [params.calleeUid]);
 
 
-//   return meetingId ? (
-//     <SafeAreaView style={{ flex: 1, backgroundColor: '#F6F6FF' }}>
-//       <MeetingProvider
-//         config={{
-//           meetingId,
-//           micEnabled: false,
-//           webcamEnabled: true,
-//           name: user.displayName || 'Test User',
-//         }}
-//         token={token}>
-//         <MeetingView autoJoin={autoJoin} setAutoJoin={setAutoJoin} />
-//       </MeetingProvider>
-//     </SafeAreaView>
-//   ) : (
-//     <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <JoinScreen getMeetingId={getMeetingId}  />
-//     </SafeAreaView>
-//   );
-// }
+  return meetingId ? (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#F6F6FF' }}>
+      <MeetingProvider
+        config={{
+          meetingId,
+          micEnabled: false,
+          webcamEnabled: true,
+          name: user.displayName || 'Test User',
+        }}
+        token={token}>
+        <MeetingView autoJoin={autoJoin} setAutoJoin={setAutoJoin} />
+      </MeetingProvider>
+    </SafeAreaView>
+  ) : (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <JoinScreen getMeetingId={getMeetingId}  />
+    </SafeAreaView>
+  );
+}
 
 
 // import React, { useState, useEffect } from 'react';
