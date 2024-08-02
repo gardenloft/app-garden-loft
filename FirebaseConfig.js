37bf67ac59aa9a67053c54e122b17c316ad95fcb
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-08-02 13:30:33 -0600

    greg's token change

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,15 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 import { getAnalytics, isSupported } from "firebase/analytics";
-//Required import to use Firebase Authentication
 import { getAuth } from "firebase/auth";
-//Required import to use Firebase Authentication
 import { getFirestore } from "firebase/firestore";
-//required for Firebase Messaging
-// import { getMessaging, getToken, onMessage } from 'firebase/messaging';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
--- app/home.js
+++ app/home.js
@@ -1,207 +1,34 @@
-// // this code give 'console warning expo token'
-
-// import "react-native-gesture-handler";
-// import { GestureHandlerRootView } from "react-native-gesture-handler";
-// import { SafeAreaProvider } from "react-native-safe-area-context";
-// import HelpButton from "../components/HelpButton";
-// import Carousel from "../components/CarouselOne/Carousel";
-// //used for deep linking video call
-// import React, { useEffect, useState, useRef } from 'react';
-// import { View, StyleSheet, Text, Platform, Alert } from 'react-native';
-// import * as Notifications from 'expo-notifications';
-// import * as Linking from 'expo-linking';
-// import Constants from 'expo-constants';
-// import { useRouter } from 'expo-router';
-// import { getAuth } from 'firebase/auth';
-// import { FIRESTORE_DB } from '../FirebaseConfig';
-// import { doc, setDoc } from 'firebase/firestore';
-// import * as Device from 'expo-device';
-// import CallAlertModal from '../components/CallAlertModal';
-
-// export default function App() {
-//   const auth = getAuth();
-//   const user = auth.currentUser;
-//   const notificationListener = useRef(null);
-//   const responseListener = useRef(null);
-//   //modal alert
-//   const [modalVisible, setModalVisible] = useState(false);
-//   const [callerName, setCallerName] = useState('');
-//   const [meetingId, setMeetingId] = useState(null);
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     if (user) {
-//       registerForPushNotificationsAsync().then(token => {
-//         if (token) {
-//           setDoc(doc(FIRESTORE_DB, 'users', user.uid), { pushToken: token }, { merge: true });
-//         }
-//       });
-
-//       Notifications.setNotificationCategoryAsync('incoming_call', [
-//         {
-//           identifier: 'ACCEPT_CALL',
-//           buttonTitle: 'Accept',
-//           options: { opensAppToForeground: true },
-//         },
-//         {
-//           identifier: 'DECLINE_CALL',
-//           buttonTitle: 'Decline',
-//           options: { opensAppToForeground: false },
-//         },
-//       ]);
-
-//       notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-//         const { caller, meetingId } = notification.request.content.data;
-//         setCallerName(caller);
-//         setMeetingId(meetingId);
-//         setModalVisible(true);
-//       });
-
-//       responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-//         const { meetingId, caller, setAutoJoinForCallee } = response.notification.request.content.data;
-//         if (response.actionIdentifier === 'ACCEPT_CALL') {
-      
-//           Linking.openURL(`app-garden-loft://VideoSDK2?meetingId=${meetingId}?caller=${caller}&sutoJoin=true`);
-    
-//         } else if (response.actionIdentifier === 'DECLINE_CALL') {
-//           // Handle decline action if needed
-//         }
-//       });
-
-//       return () => {
-//         if (notificationListener.current) {
-//           Notifications.removeNotificationSubscription(notificationListener.current);
-//         }
-//         if (responseListener.current) {
-//           Notifications.removeNotificationSubscription(responseListener.current);
-//         }
-//       };
-//     }
-//   }, [user]);
-
-//   const joinMeeting = async () => {
-//     setModalVisible(false);
-//     router.push({
-//       pathname: '/VideoSDK2',
-//       params: { meetingId, autoJoin: true }
-//     });
-
-//   };
-
-//   const handleDecline = () => {
-//     setModalVisible(false);
-//   };
-
-
-
-//   async function registerForPushNotificationsAsync() {
-//     let token;
-
-//     if (Device.isDevice) {
-//       const { status: existingStatus } = await Notifications.getPermissionsAsync();
-//       let finalStatus = existingStatus;
-//       if (existingStatus !== 'granted') {
-//         const { status } = await Notifications.requestPermissionsAsync();
-//         finalStatus = status;
-//       }
-      
-//       if (finalStatus !== 'granted') {
-//         alert('Failed to get push token for push notification!');
-//         return;
-//       }
-//       console.log('Existing status:', existingStatus);
-//       console.log('Final status:', finalStatus);
-      
-
-//       token = (await Notifications.getExpoPushTokenAsync({
-//         projectId: Constants.expoConfig?.extra?.eas?.projectId
-//       })).data;
-//       console.log("this is the token given back,", token)
-//     } else {
-//       alert('Must use physical device for Push Notifications');
-//     }
-    
-//     try {
-//       token = (await Notifications.getExpoPushTokenAsync({
-//         projectId: Constants.expoConfig?.extra?.eas?.projectId
-//       })).data;
-//       console.log('Push token:', token);
-//     } catch (error) {
-//       console.error('Error getting push token:', error);
-//     }
-
-//     if (Platform.OS === 'android') {
-//       Notifications.setNotificationChannelAsync('default', {
-//         name: 'default',
-//         importance: Notifications.AndroidImportance.MAX,
-//         vibrationPattern: [0, 250, 250, 250],
-//         lightColor: '#FF231F7C',
-//       });
-//     }
-
-//     return token;
-//   }
-
-//   return (
-//     <GestureHandlerRootView style={{ flex: 1, backgroundColor: "#FCF8E3" }}>
-//     <SafeAreaProvider>
-//       <HelpButton />
-//       <Carousel />
-//     </SafeAreaProvider>
-//     <CallAlertModal
-//         visible={modalVisible}
-//         callerId={callerName}
-//         onAccept={joinMeeting}
-//         onDecline={handleDecline}
-//       />
-//   </GestureHandlerRootView>
-//   )
-// }
-
-// this code gives 'console error' for expo token
-
 import "react-native-gesture-handler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import HelpButton from "../components/HelpButton";
 import Carousel from "../components/CarouselOne/Carousel";
+//used for deep linking video call
 import React, { useEffect, useState, useRef } from 'react';
-import { View, Platform, Alert, ActivityIndicator } from 'react-native';
+import { View, StyleSheet, Text, Platform, Alert } from 'react-native';
 import * as Notifications from 'expo-notifications';
 import * as Linking from 'expo-linking';
 import Constants from 'expo-constants';
 import { useRouter } from 'expo-router';
 import { getAuth } from 'firebase/auth';
-import { FIRESTORE_DB, FIREBASE_APP } from '../FirebaseConfig';
+import { FIRESTORE_DB, FIREBASE_AUTH } from '../FirebaseConfig';
 import { doc, setDoc } from 'firebase/firestore';
 import * as Device from 'expo-device';
 import CallAlertModal from '../components/CallAlertModal';
 
-export default function App() {
-  const [firebaseInitialized, setFirebaseInitialized] = useState(false);
+export default function Home() {
+  const auth = FIREBASE_AUTH;
+  const user = auth.currentUser;
+  const notificationListener = useRef(null);
+  const responseListener = useRef(null);
+  //modal alert
   const [modalVisible, setModalVisible] = useState(false);
   const [callerName, setCallerName] = useState('');
   const [meetingId, setMeetingId] = useState(null);
-  const notificationListener = useRef(null);
-  const responseListener = useRef(null);
   const router = useRouter();
-  const auth = getAuth(FIREBASE_APP);
-  const user = auth.currentUser;
 
   useEffect(() => {
-    const initializeFirebase = async () => {
-      try {
-        setFirebaseInitialized(true);
-      } catch (error) {
-        console.error('Firebase initialization error:', error);
-      }
-    };
-
-    initializeFirebase();
-  }, []);
-
-  useEffect(() => {
-    if (firebaseInitialized && user) {
+    if (user) {
       registerForPushNotificationsAsync().then(token => {
         if (token) {
           setDoc(doc(FIRESTORE_DB, 'users', user.uid), { pushToken: token }, { merge: true });
@@ -229,9 +56,13 @@ export default function App() {
       });
 
       responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-        const { meetingId, caller } = response.notification.request.content.data;
+        const { meetingId, caller, setAutoJoinForCallee } = response.notification.request.content.data;
         if (response.actionIdentifier === 'ACCEPT_CALL') {
-          Linking.openURL(`app-garden-loft://VideoSDK2?meetingId=${meetingId}&caller=${caller}&autoJoin=true`);
+      
+          Linking.openURL(`app-garden-loft://VideoSDK2?meetingId=${meetingId}?caller=${caller}&sutoJoin=true`);
+    
+        } else if (response.actionIdentifier === 'DECLINE_CALL') {
+          // Handle decline action if needed
         }
       });
 
@@ -244,22 +75,35 @@ export default function App() {
         }
       };
     }
-  }, [firebaseInitialized, user]);
+  }, [user]);
 
-  const joinMeeting = () => {
+  const joinMeeting = async () => {
     setModalVisible(false);
     router.push({
       pathname: '/VideoSDK2',
       params: { meetingId, autoJoin: true }
     });
+
   };
 
   const handleDecline = () => {
     setModalVisible(false);
   };
 
+
+
   async function registerForPushNotificationsAsync() {
     let token;
+
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+
 
     if (Device.isDevice) {
       const { status: existingStatus } = await Notifications.getPermissionsAsync();
@@ -268,56 +112,40 @@ export default function App() {
         const { status } = await Notifications.requestPermissionsAsync();
         finalStatus = status;
       }
-
+      
       if (finalStatus !== 'granted') {
         alert('Failed to get push token for push notification!');
         return;
       }
-
+      console.log('Existing status:', existingStatus);
+      console.log('Final status:', finalStatus);
+    }
       try {
-        token = (await Notifications.getExpoPushTokenAsync({
-          projectId: Constants.expoConfig?.extra?.eas?.projectId
-        })).data;
-        console.log('Push token:', token);
+        let theId = Constants.expoConfig?.extra?.eas?.projectId;
+        console.log("projectId is:", theId);
+        let pushToken = await Notifications.getExpoPushTokenAsync({projectId: theId});
+        console.log("pushToken is:", pushToken);
+        token = pushToken.data;
+        console.log('Token is:', token);
       } catch (error) {
         console.error('Error getting push token:', error);
       }
 
-      if (Platform.OS === 'android') {
-        Notifications.setNotificationChannelAsync('default', {
-          name: 'default',
-          importance: Notifications.AndroidImportance.MAX,
-          vibrationPattern: [0, 250, 250, 250],
-          lightColor: '#FF231F7C',
-        });
-      }
-    } else {
-      alert('Must use physical device for Push Notifications');
-    }
-
     return token;
-  }
-
-  if (!firebaseInitialized) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
-    );
   }
 
   return (
     <GestureHandlerRootView style={{ flex: 1, backgroundColor: "#FCF8E3" }}>
-      <SafeAreaProvider>
-        <HelpButton />
-        <Carousel />
-        <CallAlertModal
-          visible={modalVisible}
-          callerId={callerName}
-          onAccept={joinMeeting}
-          onDecline={handleDecline}
-        />
-      </SafeAreaProvider>
-    </GestureHandlerRootView>
-  );
+    <SafeAreaProvider>
+      <HelpButton />
+      <Carousel />
+    </SafeAreaProvider>
+    <CallAlertModal
+        visible={modalVisible}
+        callerId={callerName}
+        onAccept={joinMeeting}
+        onDecline={handleDecline}
+      />
+  </GestureHandlerRootView>
+  )
 }
